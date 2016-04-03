1d5c112a93f0c757723c12fc402cd467c87a3251
stanlee95 <bond_95@mail.ru>
2016-04-04 00:38:23 +0300

    sadsad

---
+++ IndexController.backup.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use Phalcon\MVC\Controller;
+
+class IndexController extends Controller{
+    
+    public function indexAction()
+    {
+        
+            //echo "<h1>HelloWorld</h1>";
+
+    }
+    
+}
